339c20abd762db63308dac7aa363780332ebf607
Himali Gunjal <himaligunjal31@gmail.com>
2020-07-08 22:16:18 +0530

    first commit

---
+++ JavaProject/src/assignment2_1/_1.java
@@ -0,0 +1,17 @@
+package assignment2_1;
+
+public class _1 {
+  
+	public static void main(String args[])
+	{
+		System.out.println("person Details:");
+		System.out.println("----------------");
+		System.out.println("");
+		System.out.println("First Name: Divya");
+		System.out.println("Last Name: Bharathi");
+		System.out.println("Gender: F");
+		System.out.println("Age: 20");
+		System.out.println("Weight:85.55");
+	}
+	
+}
